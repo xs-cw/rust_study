13365a8fb551e2b6d45c15b374185ab87772feea
徐尚 <xushang@100.me>
2021-05-11 09:51:08 +0800

    leetcode

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -1,5 +1,10 @@
+use rocket::http::ext::IntoCollection;
+use std::collections::HashMap;
+
 struct Solution {}
+
 impl Solution {
+    // 重复数字
     pub fn find_repeat_number(nums: Vec<i32>) -> i32 {
         let mut nums = nums.clone();
         let mut tmp = nums[0];
@@ -14,12 +19,13 @@ impl Solution {
             }
         }
     }
+    //
     pub fn is_long_pressed_name(name: String, typed: String) -> bool {
         let (name, typed) = (name.into_bytes(), typed.into_bytes());
         let mut i = 0;
         for c in typed {
-            if i >= name.len(){
-                i = name.len() -1;
+            if i >= name.len() {
+                i = name.len() - 1;
             };
             if c == name[i] {
                 i += 1;
@@ -37,10 +43,60 @@ impl Solution {
         }
         i == name.len()
     }
+    // 只出现一次的数字 II
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        let mut hs = HashMap::new();
+        for n in nums {
+            let v = hs.entry(n).or_insert(0);
+            *v += 1;
+            if *v == 3 {
+                hs.remove(&n);
+                continue;
+            }
+        }
+        hs.keys().fold(0, |_i, x| *x)
+        // for n in hs.keys() {
+        //     return *n;
+        // }
+        // 0
+    }
+    // 只出现一次的数字 II
+    pub fn single_num(nums: Vec<i32>) -> i32 {
+        nums.iter()
+            .fold((0, 0), |(once, twice), num| {
+                (
+                    (once | twice | num) & !(once & twice & num),
+                    once & (twice ^ num),
+                )
+            })
+            .0
+    }
+    // 反推补码
+    pub fn decode(encoded: Vec<i32>, first: i32) -> Vec<i32> {
+        let mut res = vec![];
+        let mut tmp = first;
+        res.reserve(encoded.len() + 1);
+        res.push(first);
+        for i in encoded {
+            tmp ^= i;
+            res.push(tmp);
+        }
+        res
+    }
 }
+
+
 fn main() {
+    let a = [6, 2, 7, 3];
+    println!("{:?}", Solution::decode(Vec::from(a), 4));
+    // the sum of all of the elements of the array
+    let sum = a.iter().fold(0, |acc, &x| {
+        println!("{},{}", acc, x);
+        acc + x
+    });
+    println!("{}", sum);
     print!(
         "{}",
-        Solution::find_repeat_number(vec![1, 2, 3, 5, 2, 4, 1])
+        Solution::single_number(vec![111, 2, 3, 3, 3, 2, 2])
     );
 }
